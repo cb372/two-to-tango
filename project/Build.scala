d5c167cfec69a7668f74da1facaad9998d8c9c4a
Chris Birchall <c-birchall@m3.com>
2013-09-19 14:34:07 +0900

    Add New Relic settings

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,8 @@ object ApplicationBuild extends Build {
     "com.typesafe"         %% "play-plugins-mailer"       % "2.1.0",
     "com.h2database"       %  "h2"                        % "1.3.173",
     "com.github.tototoshi" %% "play-flyway"               % "0.1.4",
-    "org.specs2"           %% "specs2"                    % "2.1.1"        % "test"
+    "org.specs2"           %% "specs2"                    % "2.1.1"        % "test",
+    "com.newrelic.agent.java" % "newrelic-agent"          % "2.21.4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
